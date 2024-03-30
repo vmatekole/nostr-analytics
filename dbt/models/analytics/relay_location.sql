70efbcb91a305a5e1a3a202e1fa61763fc7bd03d
Victor Matekole <victor@matekole.com>
2024-03-30 10:15:11 +0100

    feat: add dbt

---
+++ dbt/models/analytics/relay_location.sql
@@ -0,0 +1,16 @@
+-- models/combined_location.sql
+
+-- Create a new model that combines latitude and longitude into a single location column
+-- You can adjust the schema and table name as needed
+
+-- Replace `your_project` with your actual project name
+{{ config(
+  materialized='table'
+) }}
+
+with relay_location as (
+    SELECT relay_url, country_code, CONCAT(latitude, ',', longitude) AS location, CURRENT_TIMESTAMP() as inserted_at, policy.read, policy.write
+    FROM`nostr_production_data.relay`
+
+)
+select * from relay_location
